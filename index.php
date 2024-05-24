08e9422d722130061dfec090ae30221551c4ca5c
Elías Cáceres <91036133+eliascaceresalvez@users.noreply.github.com>
2024-05-24 11:48:37 -0300

    Commit Everything until now
    Commit con los cambios y distintos añadidos

---
+++ index.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Chest Gab</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" 
+        />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" crossorigin="anonymous" referrencepolicy="no-referrer"/>
+    </head>
+
+    <body>
+
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"> <i class="fas fa-utiensils"></i>Restaurant</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar05" aria-controls="navbar05" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbar05">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#inicio">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#menu">Menú del día</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#chefs">Chefs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#testimonios">Testimonio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contacto">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!--Seccion Introducción-->
+        <section id="inicio" class="container-fluid p-0">
+
+            <div class="banner-img" style="position:relative; background:url('images/slider-image1.jpg') center/cover no-repeat; height:400px; ">
+                <div class="banner-text" style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%); text-align:center" >
+                    <h1 class="text-light">La sombra</h1>
+                    <p class="text-light">Restaurant del mejor sabor casero</p>
+                    <a href="#menu" class="btn btn-primary">Ver menú</a>
+                </div>
+            </div>
+
+        </section>
+
+
+        <!--Bienvenida-->
+        <section id="id" class="container mt-4 text-center">
+
+            <div class="jumbotron bg-dark text-white">
+                    <br>
+                    <h2>Bienvenido al Restaurant</h2>
+                    <p>Descubre una experiencia culinaria única</p>
+                </br>
+            </div>
+
+        </section>
+
+        <!--Chefs-->
+        <section id="chefs" class="container mt-4 text-center">
+            <h2>Nuestros Chefs</h2>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="images/colaboradores/team-image1.jpg" alt="Chef 1" class="card-img-top">
+                        <div class="card-body">
+                            <div class="card-title">
+                                <p class="card-text">Chef Alamé</p>
+                                <div class="social-icons mt-3">
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="images/colaboradores/team-image4.jpg" alt="Chef 2" class="card-img-top">
+                        <div class="card-body">
+                            <div class="card-title">
+                                <p class="card-text">a</p>
+                                <div class="social-icons mt-3">
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+
+                    <div class="card">
+                        <img src="images/colaboradores/team-image5.jpg" alt="Chef 3" class="card-img-top">
+                        <div class="card-body">
+                            <div class="card-title">
+                                <p class="card-text">Chef Alamé</p>
+                                <div class="social-icons mt-3">
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-facebook"></i></a>                                        
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+                                    <a href="#" class="text-dark me-2"><i class="fab fa-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+        </section>
+        <br><br>
+
+        <!--Testimonios-->
+        <section id="testimonios" class="bg-light py-5">
+            <div class="container">
+                <h2 class="text-center mb-4">Testimonios</h2>
+
+                <div class="row">
+                    <div class="col-md-6 d-flex">
+                        <div class="card mb-4 w-100">
+                            <div class="card-body">
+                                <p class="card-text">Muy buena comida</p>
+                            </div>
+                            <div class="card-footer text-muted">
+                                Oscar uh
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex">
+                        <div class="card mb-4 w-100">
+                            <div class="card-body">
+                                <p class="card-text">Muy buena comida</p>
+                            </div>
+                            <div class="card-footer text-muted">
+                                Oscar uh
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+
+
+        <!--Menú-->
+        <section class="container mt-4" id="menu">
+
+            <h2 class="text-center">Menú (nuestra recomendación)</h2>
+            <br>
+            <div class="row">
+                <div class="col-lg-4 col-sm-4">
+                    <div class="card">
+                        <img src="images/menu/plato-ensalada.png" alt="Greek Salmon" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Bowl de Salmón griego</h5>
+                            <p class="card-text"><strong>Precio:</strong> $4.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <div class="card">
+                        <img src="images/menu/plato-panqueue.png" alt="Pancake Dish" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Plato panqueues con frutos</h5>
+                            <p class="card-text"><strong>Precio:</strong> $4.99</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-4">
+                    <div class="card">
+                        <img src="images/menu/plato-carne.png" alt="Meat and Vegetables" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Plato de carne con verduras</h5>
+                            <p class="card-text"><strong>Precio:</strong> $4.99</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <!--Contacto-->
+        <section class="container mt-4" id="contacto">
+
+            <h2>Contacto</h2>
+            <p>Estamos aquí para servirle</p>
+
+            <form action="?" method="post">
+                
+                <div class="mb-3">
+                    <label for="name">Nombre:</label><br>
+                    <input type="text" class="form-control" name="nombre" placeholder="Escribe tu nombre" required><br>
+                </div>
+                <div class="mb-3">
+                    <label for="email">Correo electrónico:</label>
+                    <input type="email" class="form-control" name="correo" placeholder="Escribe tu correo electrónico" required><br>
+                </div>
+                <div class="mb-3">
+                    <label for="message">Mensaje:</label><br>
+                    <textarea name="mensaje" class="form-control" id="message" rows="6" cols="50"></textarea><br>
+                </div>
+                <input type="submit" class="btn btn-primary"value="Enviar mensaje">
+            </form>
+
+        </section>
+        <br><br>
+
+        <!--Horario-->
+        <div class="text-center bg-light p-4">
+
+            <h3 class="mb-4">Horario de atención</h3>
+            
+            <div class="container-fluid">
+                
+                <div class="row">
+
+                    <div>
+                        <p><strong>Lunes a Viernes</strong></p>
+                        <p><strong>11:00 am - 10:00 pm</strong></p>
+                    </div>
+
+                    <div>
+                        <p><strong>Sabado</strong></p>
+                        <p><strong>12:00 pm - 05:00 pm</strong></p>
+                    </div>
+
+                    <div>
+                        <p><strong>Domingo</strong></p>
+                        <p><strong>07:00 am - 02:00 pm</strong></p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <!--Footer-->
+        <footer class="bg-dark text-light text-center py-1">
+            <!-- place footer here -->
+            <p> &copy; 2023 Chest Gab, todos los derechos reservados </p>
+        </footer>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
